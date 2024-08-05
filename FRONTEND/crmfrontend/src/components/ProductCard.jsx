083717f75d492b460d8c0ca1d693f7ebe28fc4e4
muthumanikandan390d <muthumanikandan390d@gmail.com>
2024-08-05 09:26:46 +0530

    img-added-play

--- FRONTEND/crmfrontend/src/components/ProductCard.jsx
+++ FRONTEND/crmfrontend/src/components/ProductCard.jsx
@@ -1,9 +1,17 @@
 /* eslint-disable react/prop-types */
 import styles from './ProductCard.module.css';
+const name = "playstation"
+const price = "20$"
+const imgs  = "https://media.istockphoto.com/id/1451456915/photo/female-freelance-developer-coding-and-programming-coding-on-two-with-screens-with-code.webp?b=1&s=170667a&w=0&k=20&c=7QL_-R6HTGHCYt37C8NyjuW6mIiJeQhDVgW8i_6l4Vk="
+
 function ProductCard() {
   return (
     <div className={styles.card}>
-      hemlo
+       <img src={imgs} alt={name} className={styles.image} />
+      <div className={styles.details}>
+        <span className={styles.name}>{name}</span>
+        <span className={styles.price}>{price}</span>
+      </div>
     </div>
   )
 }
--- FRONTEND/crmfrontend/src/pages/HomePage.jsx
+++ FRONTEND/crmfrontend/src/pages/HomePage.jsx
@@ -78,6 +78,9 @@ function HomePage() {
                   <ProductCard />
                   <ProductCard />
                   <ProductCard />
+                  <ProductCard />
+                  <ProductCard />
+                  <ProductCard />
 
                 </div>
 
