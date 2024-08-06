0a237faef38475dd9c29504cf92e4753d6fe43dd
muthumanikandan390d <muthumanikandan390d@gmail.com>
2024-08-06 18:47:19 +0530

    psyco-pg2

--- BACKEND/crmbackend/crmbackend/settings.py
+++ BACKEND/crmbackend/crmbackend/settings.py
@@ -73,13 +73,18 @@ WSGI_APPLICATION = 'crmbackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+database_name = 'crmdb'
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': database_name,
+        'USER': 'postgres',
+        'PASSWORD': 'cosaimp@2020',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
